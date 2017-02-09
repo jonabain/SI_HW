e1a3abc0193810a469125efad9ea6eb1860201a6
Jonathan Bain <jonabain@umich.edu>
2017-02-09 12:52:34 -0500

    Update SI206W17_HW4.py

--- SI206W17_HW4.py
+++ SI206W17_HW4.py
@@ -88,18 +88,20 @@ soup = BeautifulSoup(htmldoc,"html.parser")
 people = soup.find_all("div",{"class":"views-row"})
 umsi_titles = {}
 ## It may be helpful to translate the following from English to code:
-
+print(len(people))
+count = 0;
 ## For each element in the list saved in the variable people,
 ## Find the container that holds the name that belongs to that person (HINT: look for something unique, like a property element...)
 for row in people:
-	name = row.find("meta", property="dc:title")
+	name = row.find(property="dc:title")
 ## Find the container that holds the title that belongs to that person (HINT: a class name)
-	title = row.find("div", attrs={'class' : 'field-item even'})
+	title = row.find("div", attrs={'class' : 'field-name-field-person-titles'})
 ## Grab the text of each of those elements and put them in the dictionary umsi_titles properly
-	if type(name) and type(title) == type('yes'):
-		umsi_titles = {name:title}
-		name = None
-		title = None
+	umsi_titles[name.text] = title.text
+	name = None
+	title = None
+
+# print((umsi_titles))
 
 ######### UNIT TESTS; DO NOT CHANGE ANY CODE BELOW THIS LINE #########
 #### NOTE: hard-coding to pass any of these tests w/o following assignment instructions is not acceptable for points
